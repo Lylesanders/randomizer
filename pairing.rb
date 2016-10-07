47bca8a0ebbfa085741cd2d12e9252dbddc7e0fb
lyle Sanders <lylesanders66@yahoo.com>
2016-10-07 10:28:55 -0400

    first commit

---
+++ pairing.rb
@@ -0,0 +1,7 @@
+
+
+
+def name_array(array)
+	array.shuffle.each_slice(2).to_a
+			
+end
---
+++ test_pairing.rb
@@ -0,0 +1,27 @@
+require "minitest/autorun"						# add minitest function to program
+require_relative "pairing.rb"					# point minitest to division.rb
+
+class TestPairing < Minitest::Test  	#create template for test
+
+	def test_one_cent
+		 assert_equal(1,1)
+	end
+
+	def test_for_empty_array
+		array = []
+		assert_equal([], name_array(array))
+	end # test for empty array	
+
+def test_for_pairing_out_four_names
+	array = name_array(["susan", "joe", "cyndi", "marv"])
+	assert_equal(2, array.count)
+end
+
+def test_for_pairing_out_six_names
+	array = name_array(["susan", "joe", "cyndi", "marv","jeremy","jacob"])
+	assert_equal(3, array.count)	
+end
+
+
+
+end
